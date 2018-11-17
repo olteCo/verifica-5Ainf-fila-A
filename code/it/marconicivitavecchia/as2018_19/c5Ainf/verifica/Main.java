a759019ce64e5cc3eadc0fe9950c967d86ec5da0
olteCo <olteanu15.co@gmail.com>
2018-11-17 10:21:03 +0100

    Consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
@@ -12,16 +12,47 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(400,700);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
+		StringBuilder sb=new StringBuilder();
+		sb.append("<html>");
+		sb.append("<h1>SSH First Aid </h1>");
+		sb.append("<div style='display:table'>");
+		//Prima riga Alians HostName
+		sb.append("<div style='display:table-row'>");
 		
-		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<div style='display:table-cell'>");
+		sb.append("Alians");
+		sb.append("HostName");
+		sb.append("</div>");
+		sb.append("</div>");
 		
-		// TODO Convertire lo StringBuilder in String
+		//Prima riga 
+
+		sb.append("<div style='display:table-row'>");
+		sb.append("<div style='display:table-cell'>");
+		sb.append("	Raspberry");
+		sb.append("server.marconi.it");
+		sb.append("</div>");
+		sb.append("</div>");
 		
-		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		//Seconda riga 
+
+		sb.append("<div style='display:table-row'>");
+		sb.append("<div style='display:table-cell'>");
+		sb.append("CoreSwitch");
+		sb.append("10.0.100.1");
+		sb.append("</div>");
+		sb.append("</div>");
+		
+		//chiusura div table
+		sb.append("</div>");
+		sb.append("</html>");
+		
+		
+		String lbl=sb.toString();
+		super.add(new JLabel(lbl));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
